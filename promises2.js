d5b3b253aab035d6a38b3e553b01410b9977bf5c
Prakash Anand <ppa58514@gmail.com>
2020-08-10 11:54:18 +0530

    promises with then

---
+++ promises2.js
@@ -0,0 +1,18 @@
+let prom = new Promise((resolve, reject) => {
+  let num = Math.random();
+  if (num < .5 ){
+    resolve('Yay!');
+  } else {
+    reject('Ohhh noooo!');
+  }
+});
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+
+prom.then(handleSuccess, handleFailure);

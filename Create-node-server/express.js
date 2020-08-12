eee8ae33e62233a03495e6da7a42e48dba2d3733
Prakash Anand <ppa58514@gmail.com>
2020-08-12 23:02:44 +0530

    Dynamic routing using route parameter

--- Create-node-server/express.js
+++ Create-node-server/express.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 4001;
 app.get("/",(req,res)=>{
    res.send("Hello");
 })
+const monsters = { hydra: { height: 3, age: 4 }, dragon: { height: 200, age: 350 } };
+// GET /monsters/hydra
+app.get('/monsters/:name', (req, res, next) => {
+  console.log(req.params) // { name: 'hydra' };
+  res.send(monsters[req.params.name]);
+});
 
 // Invoke the app's `.listen()` method below:
 app.listen(PORT, () => {

6d38c2ab98487bbea85c6f3d0bb77771ecf85dda
Prakash Anand <ppa58514@gmail.com>
2020-08-12 23:29:06 +0530

    setting status code manually

--- Create-node-server/express.js
+++ Create-node-server/express.js
@@ -5,7 +5,7 @@ const app =  express();
 
 const PORT = process.env.PORT || 4001;
 
-const monsters = { hydra: { height: 3, age: 4 }, dragon: { height: 200, age: 350 } };
+const monsters = { hydra: { height: 3, age: 4 }, dragon: { height: 200, age: 350 },thanos: { height: 9, age: 41 } };
 app.get("/",(req,res)=>{
    res.send("Hello");
 })
@@ -13,7 +13,10 @@ app.get("/",(req,res)=>{
 // Dynamic Routing
 app.get('/monsters/:name', (req, res, next) => {
   console.log(req.params) // 
+  if(req.params.name)
   res.send(monsters[req.params.name]);
+  else
+  res.status(404).send('Monster not found');
 });
 
 // Invoke the app's `.listen()` method below:

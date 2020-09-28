bf941051716441f99e5021f7863f131ba9e232c2
PRAKASH ANAND <34940322+GHATAK123@users.noreply.github.com>
2020-09-28 19:59:59 +0530

    Create http.js

---
+++ Create-node-server/http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+let requestListener = (request, response) => {
+  
+  response.write('Hello World!\n');
+  response.end();
+  
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(3000);
